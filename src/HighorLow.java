c095afd64db60657e1e6fee992b509551dac6df0
Guptan0506 <navyag0509@gmail.com>
2024-10-04 17:43:19 -0400

    initial commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -5,19 +5,18 @@ public class HighorLow {
     public static void main(String [] args){
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
-        int number = rand.nextInt(10)+ 1;
+        int number = rand.nextInt(10);
         System.out.println("Guess a number between 1 to 10");
         int guess = in.nextInt();
         do {
-            System.out.println("Guess a number between 1 and 10: ");
-            while (!in.hasNextInt()){
-                System.out.println("Please enter a valid number between 1 and 10");
-                in.next();
-            }
             if (guess < number ){
                 System.out.println("Your guess was too low.");
+                System.out.println("Guess a number between 1 and 10: ");
+                in.next();
             }else if (guess > number){
                 System.out.println("Your guess was too high");
+                System.out.println("Guess a number between 1 and 10: ");
+                in.next();
             }else {
                 System.out.println("Your guess is right!");
             }
