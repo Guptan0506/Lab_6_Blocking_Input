66fda01f3337f71cd77850b9ba4395e20ad6c114
Guptan0506 <navyag0509@gmail.com>
2024-10-04 13:47:39 -0400

    initial commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 public class CtoFConverter {
-    public static void main(){
+    public static void main(String [] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Input the temperature in Celsius(C)");
-        double tempInC = in.nextDouble();
-        double tempInF = 0.00;
-        tempInF = (tempInC*(9/5)) + 32;
-        System.out.println(tempInC + " is "+ tempInF +"in Fahrenheit");
+        boolean done=false;
+        do {
+            System.out.println("Input the temperature in Celsius(C)");
+            if(in.hasNextDouble()){
+                double tempInC = in.nextDouble();
+                double tempInF = 0.00;
+                tempInF = (tempInC * (9 / 5)) + 32;
+                System.out.println(tempInC + " is " + tempInF + " in Fahrenheit");
+                done=true;
+            }
+            else{
+                System.out.println("invalid input");
+                in.nextLine();
+            }
+
+        }while(!done);
     }
 }
--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 public class FuelCosts {
+    public void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        do {
+            System.out.println("Enter the number of gallons of gas in tank");
+            double numberOfGallons = in.nextDouble();
+            System.out.println("Enter the fuel efficiency in miles per gallon");
+            double fuelEfficiency = in.nextDouble();
+            System.out.println("Enter the price per gallon");
+            double priceOfGallon = in.nextDouble();
+        }while(!done);
+    }
 }
